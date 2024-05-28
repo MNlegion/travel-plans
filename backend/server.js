02fb2a15b1ebc18bb531ed70167f86ce060479c1
MNlegion <rkreuser30@gmail.com>
2024-05-27 21:39:53 -0400

    continued project setup

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
+const port = process.env.PORT || 3000;
 
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
 
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 }); 
