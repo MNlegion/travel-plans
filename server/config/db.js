8284fe4ac4a12e946ee86f284c2c69f38a464466
MNlegion <rkreuser30@gmail.com>
2024-05-30 22:46:45 -0400

    continued app initial setup

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("MongoDB connected");
+    console.log(`MongoDB connected to: ${conn.connection.host}`.bgBlack.magenta);
   } catch (error) {
     console.error(error.message);
     process.exit(1);
